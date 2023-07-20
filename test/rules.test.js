7d839cfc5e805a30c75564b7148602331f8657b4
Max Hurlimann <hurlimannmax@gmail.com>
2023-07-20 08:10:40 +0200

    Write unit test for subgridWinner function

--- test/rules.test.js
+++ test/rules.test.js
@@ -1,5 +1,37 @@
-const sum = (a, b) => a+b;
+import { SubGrid } from "../modules/grid.js";
+import { Player } from "../modules/player.js";
+import { subgridWinner } from "../modules/rules";
 
-test("add two numbers", () => {
-    expect(sum(1,2)).toBe(3);
+test("correctly determine subgrid winner", () => {
+    let subgrid1 = new SubGrid();
+    subgrid1.grid = 
+       [Player.None, Player.None, Player.None, 
+        Player.None, Player.None, Player.None, 
+        Player.None, Player.None, Player.None ];
+    
+    expect(subgridWinner(subgrid1.grid)).toBe(Player.None)
+
+    let subgrid2 = new SubGrid();
+    subgrid2.grid = 
+       [Player.X, Player.None, Player.None, 
+        Player.X, Player.X,    Player.None, 
+        Player.O, Player.O,    Player.X ];
+    
+    expect(subgridWinner(subgrid2.grid)).toBe(Player.X)
+
+    let subgrid3 = new SubGrid();
+    subgrid3.grid = 
+       [Player.O,    Player.O,    Player.O, 
+        Player.None, Player.None, Player.None, 
+        Player.None, Player.None, Player.None ];
+    
+    expect(subgridWinner(subgrid3.grid)).toBe(Player.O)
+
+    let subgrid4 = new SubGrid();
+    subgrid4.grid = 
+       [Player.X, Player.X, Player.O, 
+        Player.O, Player.O, Player.X, 
+        Player.X, Player.O, Player.X ];
+    
+    expect(subgridWinner(subgrid4.grid)).toBe(Player.Tie)
 });
