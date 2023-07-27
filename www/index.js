ebc3b0c48cd1640467fe168ed1c84486a6ee2d66
AzTekG <88374025+AzTekG@users.noreply.github.com>
2023-07-27 16:19:15 +0200

    Implement socket logic and bugfix
    Bug was that socket.on('move'...) was calling makeMove with characters
    rather than integers

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 //ideally would like to have a game.js module that does all the interacting.
 //haven't figured out how to do that
 
-import { createGrid, makeMove } from "./modules/grid.js";
+import { createGrid, makeMove, yourTurn, } from "./modules/grid.js";
 
 
 createGrid(document.getElementById('grid'));
@@ -20,10 +20,34 @@ socket.on('full', () => {
     alert("Game is full");
     location.reload();
 });
+socket.on('waiting for player', () => {
+    //WE ARE X
+    yourTurn(); //TODO CHANGE
+    console.log("waiting for player");
+});
+
+socket.on('matched with player', () => {
+    //WE ARE O
+    console.log("matched with player");
+});
+
+socket.on('start', () => {
+    console.log("start");
+});
+
+socket.on('move', move => {
+    makeMove(parseInt(move[0]), parseInt(move[1]), parseInt(move[2]), parseInt(move[3]), true)();
+    yourTurn();
+    
+});
+
+function getSocket() {
+    return socket;
+}
 
 //why do i need to do this..
 window.makeMove = makeMove;
 window.reset = reset;
 
 
-
+export { getSocket };
