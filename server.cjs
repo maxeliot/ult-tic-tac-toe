b67b2fa988916fad555e7f9e221f9fc3b850ff85
AzTekG <88374025+AzTekG@users.noreply.github.com>
2023-07-27 20:46:11 +0200

    Use process.env.PORT for azure deploy

--- server.cjs
+++ server.cjs
@@ -45,6 +45,6 @@ io.on('connection', (socket) => {
      
 });
 
-server.listen(3000, () => {
-  console.log("App available on http://localhost:3000");
+server.listen(process.env.PORT || '3000', () => {
+  console.log(`App available on http://localhost:${process.env.PORT || '3000'}`);
 });
